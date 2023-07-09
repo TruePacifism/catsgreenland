069121ca9878a2acc21eb2c2f8650e6040b3cc5f
TruePacifism <76737760+TruePacifism@users.noreply.github.com>
2023-07-09 04:31:57 +0300

    Update DeleteIcon.jsx

--- src/components/UserInfoEdit/DeletingForms/DeleteIcon/DeleteIcon.jsx
+++ src/components/UserInfoEdit/DeletingForms/DeleteIcon/DeleteIcon.jsx
@@ -1,6 +1,6 @@
-import { ReactComponent as DeleteIcon } from '../../../../images/delete-icon.svg';
+import { ReactComponent as Icon } from '../../../../images/delete-icon.svg';
 import styles from './DeleteIcon.module.css';
 
 export default function DeleteIcon({ action }) {
-  return <DeleteIcon className={styles.icon} onClick={action} />;
+  return <Icon className={styles.icon} onClick={action} />;
 }
